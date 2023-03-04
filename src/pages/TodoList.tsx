de84ce7ff1a4633fb83a4feafb8f374927ccfa47
hana1203 <hanaindec@gmail.com>
2023-03-05 00:01:02 +0900

    fix: delete, update시 비동기 순서로 한발짝 느리게 렌더링되는 이슈 해결

--- src/pages/TodoList.tsx
+++ src/pages/TodoList.tsx
@@ -1,43 +1,42 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import { createTodo, deleteTodo, getTodos, updateTodo } from "../apis/todo";
+import { createTodo, getTodos } from "../apis/todo";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
+import { TodoObj } from "../types/type";
 import { TodoRow } from "./TodoRow";
 
-export const Todo = () => {
+export const TodoList = () => {
   const [todoInput, setTodoInput] = useState("");
-  const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useState<TodoObj[]>();
   const [isLIstUpdated, setIsLIstUpdated] = useState(false);
-
-  // const [editId, setEditId] = useState(null);
-  // const [edittedInput, setEdittedInput] = useState();
-
-  // const [isChecked, setIsChecked] = useState();
 
   const navigate = useNavigate();
   const submitTodo = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     createTodo({ todoBody: todoInput });
-    setIsLIstUpdated(true);
     setTodoInput(""); //추가시 input 값 비우기
+    setIsLIstUpdated(true);
   };
   console.log(todoInput);
 
   useEffect(() => {
-    // getTodos().then((aDataObj) => setTodoList(aDataObj));
-    getTodos({ setTodoList });
+    // getTodos({ setTodoList });
+    // setIsLIstUpdated(false);
+    getTodos().then((datalist) => setTodoList(datalist));
     setIsLIstUpdated(false);
+  }, [isLIstUpdated]);
+  console.log("todoList상태", todoList); // [{…}, {…}]
+  // [{id: 8717, todo: '할일', isCompleted: false, userId: 4840},
+  // {id: 8718, todo: '할일2', isCompleted: false, userId: 4840}]
 
-    //토큰없는 상태로 접속시 로그인페이지로 보내기
+  //토큰없는 상태로 접속시 로그인페이지로 보내기
+  useEffect(() => {
     if (!localStorage.getItem("accessToken")) {
       navigate("/");
     }
-  }, [isLIstUpdated]);
-  console.log("todoList", todoList); // [{…}, {…}]
-  // [{id: 8717, todo: '할일', isCompleted: false, userId: 4840},
-  // {id: 8718, todo: '할일2', isCompleted: false, userId: 4840}]
+  }, []);
 
   return (
     <TodoInputListContainer>
--- src/pages/TodoRow.tsx
+++ src/pages/TodoRow.tsx
@@ -2,14 +2,16 @@ import { useState } from "react";
 import styled, { css } from "styled-components";
 import { deleteTodo, updateTodo } from "../apis/todo";
 import { Button } from "../components/Button";
-interface Props {
-  id: string;
-  todo: string;
-  isCompleted: boolean;
+import { TodoObj } from "../types/type";
+interface SetUpdateStateProps {
   setIsLIstUpdated: React.Dispatch<React.SetStateAction<boolean>>;
 }
-
-export const TodoRow = ({ id, todo, isCompleted, setIsLIstUpdated }: Props) => {
+export const TodoRow = ({
+  id,
+  todo,
+  isCompleted,
+  setIsLIstUpdated,
+}: TodoObj & SetUpdateStateProps) => {
   const [editId, setEditId] = useState<string | null>(null);
   const [edittedInput, setEdittedInput] = useState<string>(todo);
   const [isChecked, setIsChecked] = useState<boolean>(isCompleted);
@@ -20,8 +22,10 @@ export const TodoRow = ({ id, todo, isCompleted, setIsLIstUpdated }: Props) => {
   };
   //삭제 누르기
   const handleDelete = (id: string) => {
-    deleteTodo(id);
-    setIsLIstUpdated(true);
+    deleteTodo(id).then(() => setIsLIstUpdated(true)); //delete 서버에보내고 성공하면 리스트에띄울 수있게 상태 변경
+    // deleteTodo(id) //이렇게짜면 삭제 연속으로 누를때 한발짝 느리게 삭제되는 경우있음
+    //deleteTodo 비동기코드 실행되는것 보다 아래 상태가 먼저 변경되서 TodoList컴포넌트에 get요청이 먼저들어감
+    // setIsLIstUpdated(true);
   };
 
   //input에 수정한 내용 제출하기
@@ -30,14 +34,15 @@ export const TodoRow = ({ id, todo, isCompleted, setIsLIstUpdated }: Props) => {
     edittedInput: string,
     isChecked: boolean
   ) => {
-    updateTodo(id, edittedInput, isChecked);
-    setIsLIstUpdated(true);
+    updateTodo(id, edittedInput, isChecked).then(() => setIsLIstUpdated(true));
     setEditId(null);
   };
 
   //수정하던거 취소하기
   const cancelEdittedInput = (id: string) => {
     setEditId(null);
+    setEdittedInput(todo); //다시 원래 todo값으로 돌려놓기
+    setIsChecked(isCompleted); //원래 체크값으로 돌려놓기
   };
   //수정 input 값 상태에 저장
   const handleEdittedChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -49,13 +54,13 @@ export const TodoRow = ({ id, todo, isCompleted, setIsLIstUpdated }: Props) => {
     <TodoWrapper key={id}>
       <CheckBox
         type="checkbox"
-        defaultChecked={isCompleted}
+        checked={isChecked}
         onChange={(e) => setIsChecked(e.target.checked)}
       />
       {editId === id ? (
         <>
           <TodoEditContent
-            defaultValue={todo}
+            value={edittedInput}
             onChange={handleEdittedChange}
           ></TodoEditContent>
           <Button
@@ -95,7 +100,7 @@ const CheckBox = styled.input`
   cursor: pointer;
 `;
 const TodoContent = styled.div`
-  background-color: cornsilk;
+  background-color: #eeffe1;
   width: 14rem;
   /* height: 2rem; */
   height: auto;
